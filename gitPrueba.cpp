08d41eda9dca02ee9239ce88a0936daa92e69740
Lufrauwu <lexius002@gmail.com>
2021-06-09 09:43:47 -0500

    Luis

---
+++ gitPrueba.cpp
@@ -0,0 +1,9 @@
+// gitPrueba.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World! xdxd\n";
+}
